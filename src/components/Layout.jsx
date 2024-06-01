10d7a2a287f9cc166eb5058e2cc9dbd99cc55acb
Sssevil <zhunushova29@gmail.com>
2024-06-01 18:50:45 +0600

    исправления

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import NavBar from './Navbar'
 import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Layout = () => {
+    const theme = useSelector((state) => state.theme)
     return (
-        <>
+        <div style={{ height: '100vh' }} className={theme}>
             <header>
                 <NavBar />
             </header>
             <main>
                 <Outlet />
             </main>
-        </>
+        </div>
     )
 }
 
